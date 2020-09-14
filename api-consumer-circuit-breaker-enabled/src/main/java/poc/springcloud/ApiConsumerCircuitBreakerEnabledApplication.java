84bcf0ddb3ca9e2664c6065a250b45cabbe88dea
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-14 09:43:32 +0530

    api-consumer-circuit-breaker-enabled: Initial commit

---
+++ api-consumer-circuit-breaker-enabled/src/main/java/poc/springcloud/ApiConsumerCircuitBreakerEnabledApplication.java
@@ -0,0 +1,13 @@
+package poc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsumerCircuitBreakerEnabledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsumerCircuitBreakerEnabledApplication.class, args);
+	}
+
+}
